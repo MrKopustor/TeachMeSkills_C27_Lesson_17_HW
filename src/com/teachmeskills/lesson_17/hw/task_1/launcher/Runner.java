cbfc0afbf8940d7071535c741fd80fdf4ba9f0b7
mr.rabukhin <zotop1753@yahoo.com>
2024-03-17 17:53:04 +0300

    Update

--- src/com/teachmeskills/lesson_17/hw/task_1/launcher/Runner.java
+++ src/com/teachmeskills/lesson_17/hw/task_1/launcher/Runner.java
@@ -1,2 +1,42 @@
-package com.teachmeskills.lesson_17.hw.task_1.launcher;public class Runner {
+package com.teachmeskills.lesson_17.hw.task_1.launcher;
+
+import com.teachmeskills.lesson_17.hw.task_1.MyFunc;
+
+import java.util.Scanner;
+
+public class Runner {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number 1 & 2");
+
+
+        while (true) {
+            String s = scanner.nextLine();
+            if (s.equalsIgnoreCase("1")) {
+                MyFunc<String> my1;
+                my1 = (str) -> {
+                    StringBuffer buffer = new StringBuffer(str);
+                    buffer.reverse();
+                    return buffer.toString();
+                };
+
+                System.out.println(my1.doSomething("Ruslan")); // => nalsuR
+                break;
+            } else if (s.equalsIgnoreCase("2")) {
+                MyFunc<Integer> my;
+                my = (i) -> {
+                    int result = 1;
+                    for (int j = 1; j <= i; j++) {
+                        result = result * j;
+                    }
+                    return result;
+                };
+
+                System.out.println(my.doSomething(10));
+                break;
+            } else {
+                System.out.println("Invalid value, enter number 1 & 2");
+            }
+        }
+    }
 }
--- src/com/teachmeskills/lesson_17/hw/task_2/Runner.java
+++ src/com/teachmeskills/lesson_17/hw/task_2/Runner.java
@@ -1,2 +1,19 @@
-package com.teachmeskills.lesson_17.hw.task_2;public class Runner {
+package com.teachmeskills.lesson_17.hw.task_2;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class Runner {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        Locale langRu = new Locale("ru");
+
+        DayOfWeek day = date.getDayOfWeek();
+        String str = day.getDisplayName(TextStyle.FULL, langRu);
+
+        System.out.println(day.getValue()); // => 2
+        System.out.println(str); // => вторник
+    }
 }
